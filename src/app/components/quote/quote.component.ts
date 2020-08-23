22e23f2659000358a33ed99c8b677f0f1a41a9d9
Timothy Munene <munenegicharu@gmail.com>
2020-08-23 14:43:15 +0300

    Initial commit

---
+++ src/app/components/quote/quote.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../../models/Quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+  quotes: Quote[] = [
+    new Quote('First quote', 'First description', new Date(2020,3,14)),
+    new Quote('Second quote', 'Second description', new Date(2030,4,12)),
+    new Quote('Third quote', 'Third description', new Date(2020,9,13))
+  ];
+
+  toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
+  completeGoal(isComplete, index){
+    if (isComplete) {
+      this.quotes.splice(index, 1)
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
